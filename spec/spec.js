6f92a11db33fcd7598053a3a4da28a4799f4ca73
brandon <josueknd@hotmail.es>
2015-01-20 12:26:55 -0600

    funciones agregadas

--- spec/spec.js
+++ spec/spec.js
@@ -1,7 +1,28 @@
 describe("First jasmine project", function() {
   it("should say hello jasmine", function() {
 
-    expect(helloWorld()).toBe("hello world");
+	expect(helloWorld()).toBe("hello world");
+  });
+  it("should write datable on the page", function() {
+
+	expect(title).toBe("Learning Jasmine");
+	expect(title).not.toBe(" ");
   });
 
+  it("should include in the tittle Jasmine", function(){
+
+	expect(title).toMatch(/JaSmiNe/i);
+
+  });
+
+  it("verificacion de suma", function(){
+	expect(Suma(2,2)).toBe(4);
+	expect(Suma(5,"4")).toBe(9);
+	expect(Suma(5,"4d")).toBe(null);
+	expect(Suma(-50,1000)).toBe(950);
+
+  });
 });
+
+/*toBeGreaterThan
+toBelessThan*/
--- src/script.js
+++ src/script.js
@@ -1,3 +1,22 @@
+var title = "Learning Jasmine";
+
 function helloWorld() {
   return "hello world";
 }
+
+function Titulo(){
+document.write("<h1>"+title+"</h1>");
+
+};
+
+Titulo();
+function Suma(numero1,numero2){
+
+	if(isNaN(numero1)||isNaN(numero2)){
+		return null;
+	}else{
+		numero1 = parseInt(numero1);
+		numero2 = parseInt(numero2)
+		return numero1+numero2;
+	}
+};
